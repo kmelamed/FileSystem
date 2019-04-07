ff8c33d3bfe83c5862a11b0ff693744f87ee73e0
pfelix1201 <42977605+pfelix1201@users.noreply.github.com>
2019-04-06 23:25:15 -0700

    Should be finished

--- terminal.java
+++ terminal.java
@@ -1,32 +1,77 @@
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Terminal {
-	
-	public static String[] commands = {"cd", "edit", "exit", "ls", "mkdir", "rm", "rmdir", "touch"};
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.print(FileSystem.currentDirName() + " user$ ");
-		String commandLine = in.nextLine();
+//	public static String[] commands = { "cd", "edit", "exit", "ls", "mkdir", "rm", "rmdir", "touch" };
+
+	public static void main(String[] args) throws FileNotFoundException {
+		String path  = "src/doritOS";
+		File doritOS = new File(path);
+		Scanner inFile = new Scanner(doritOS);
+		String dir = inFile.nextLine();
+		inFile.close();
+		
+		FileSystem fs = new FileSystem(dir);
+		Scanner inUser = new Scanner(System.in);
+		System.out.print("computer:" + fs.currentDirName() + " user$ ");
+		
+		String commandLine = inUser.nextLine();
+		commandLine = commandLine.replace("\\ ", "\\");
 		StringTokenizer st = new StringTokenizer(commandLine);
 		String command = st.nextToken();
-//		System.out.println(command);
 		while (!command.equals("exit")) {
-			if (Arrays.asList(commands).contains(command)) {
-				switch(command) {
-					
-				}
-			} else {
-				System.out.println("-doritOS: " + command + ": command not found");
+			switch (command) {
+				case "cd":
+					if (st.countTokens() >= 1) {
+						fs.cd(st.nextToken().replace("\\", " "));
+					}
+					break;
+				case "edit":
+					if (st.countTokens() >= 2) {
+						fs.edit(st.nextToken().replace("\\", " "), st.nextToken().replace("\\", " "));					
+					}
+					break;
+				case "ls":
+					fs.ls();
+					break;
+				case "mkdir":
+					if (st.countTokens() >= 1) {
+						fs.mkdir(st.nextToken().replace("\\", " "));
+					}
+					break;
+				case "rm":
+					if (st.countTokens() >= 1) {
+						fs.rm(st.nextToken().replace("\\", " "));
+					}
+					break;
+				case "rmdir":
+					if (st.countTokens() >= 1) {
+						fs.rmdir(st.nextToken().replace("\\", " "));
+					}
+					break;
+				case "touch":
+					if (st.countTokens() >= 1) {
+						fs.touch(st.nextToken().replace("\\", " "));
+					}
+					break;
+				default:
+					System.out.println("-doritOS: " + command + ": command not found");
 			}
-			System.out.print(FileSystem.currentDirName() + " user$ ");
-			commandLine = in.nextLine();
+			System.out.print("computer:" + fs.currentDirName() + " user$ ");
+			commandLine = inUser.nextLine();
+			commandLine = commandLine.replace("\\ ", "\\");
 			st = new StringTokenizer(commandLine);
 			command = st.nextToken();
 		}
-		in.close();
+		doritOS.delete();
+		PrintWriter pw = new PrintWriter(path);
+		pw.println(fs.dir);
+		pw.close();
+		inUser.close();
 	}
 
 }
